e0ecf2911c0a566bef60c5f6e95e86337661f45f
LilianeTop <hallo@lilianetop.nl>
2023-10-30 22:02:16 +0100

    added relationship between entities

--- src/main/java/nl/lilianetop/spring6/domain/Book.java
+++ src/main/java/nl/lilianetop/spring6/domain/Book.java
@@ -1,4 +1,21 @@
 package nl.lilianetop.spring6.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String isbn;
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> authors;
+
 }
